4b434db7ed1e138f88a75f4aaa3ccd9e45c06902
b5strbal <strennerb@gmail.com>
2014-08-05 10:57:09 -0500

    Update escher.py

--- escher.py
+++ escher.py
@@ -1,3 +1,18 @@
+#*****************************************************************************
+#       Copyright (C) 2014 Balazs Strenner <strenner@math.wisc.edu>,
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
 """
 Written by: Balazs Strenner, UW-Madison, Mathematics Department
 Initial version in C++ in 2006.
